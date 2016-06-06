f6f836f48a6114ddf2c1db2edf48d1ad94fcfece
Yuchen Huang <oocasthy@umich.edu>
2016-06-06 19:55:53 -0400

    Keyboard Debug Binding Tool: Map alphabet keys to external functions for debugging Used to avoid re-binding of debugging keys in developing stage

---
+++ Debugger/KeyboardDebugger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// TODO: editor support to bind function with key via GUI
+
+/// <summary>
+/// Map alphabet keys to external functions for debugging
+/// Used to avoid re-binding of debugging keys in developing stage
+/// </summary>
+public class KeyboardDebugger : MonoBehaviour
+{
+    public delegate void BindingAction();
+    public BindingAction[] keyboardEvents = new BindingAction[26];
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    for (int i = 0; i < 26; i++)
+        {
+            if (keyboardEvents[i] != null && Input.GetKeyDown(KeyCode.A + i))
+            {
+                Debug.Log("Key " + (char)('a' + i) + " pressed");
+                keyboardEvents[i]();
+            }
+        }
+	}
+}
