a305835797e4499b501e454f8a69166e7210cb6e
Yuchen Huang <oocasthy@umich.edu>
2016-07-09 18:08:32 -0400

    Add GameStateController, ver 1.04

---
+++ GameStateController/Scripts/GameStateController.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections.Generic;
+using System;
+
+public class GameStateController : MonoBehaviour, ISerializationCallbackReceiver
+{
+    [System.Serializable]
+    public class StateEntry
+    {
+        public string stateName;
+        public UnityEvent stateCallbackFunctions;
+    }
+
+    public List<StateEntry> gameStates;
+    Dictionary<string, int> lookupTable = new Dictionary<string, int>();
+
+    public void CallStateEvent(string stateName)
+    {
+        if (lookupTable == null)
+        {
+            return;
+        }
+        int index = -1;
+        if (lookupTable.ContainsKey(stateName))
+        {
+            index = lookupTable[stateName];
+        }
+        if (index < 0)
+        {
+            Debug.Log("Statename not found");
+        }
+        else
+        {
+            gameStates[index].stateCallbackFunctions.Invoke();
+        }
+    }
+
+    public void OnBeforeSerialize()
+    {
+        // Keep empty
+    }
+
+    public void OnAfterDeserialize()
+    {
+        if (lookupTable == null)
+        {
+            lookupTable = new Dictionary<string, int>();
+        }
+        for (int i = 0; i < gameStates.Count; i++)
+        {
+            lookupTable[gameStates[i].stateName] = i;
+        }
+    }
+}
---
+++ GameStateController/Scripts/GameStateControllerTest.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameStateControllerTest : MonoBehaviour {
+    GameStateController controller;
+
+    // Use this for initialization
+    void Start () {
+        controller = GameObject.Find("GameStateController").GetComponent<GameStateController>();
+        controller.CallStateEvent("BattleStart");
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        controller.CallStateEvent("OnBattleStart");
+    }
+
+    public void BattleStart()
+    {
+        Debug.Log("Debug start");
+    }
+
+    void PrivateFunction()
+    {
+
+    }
+}
